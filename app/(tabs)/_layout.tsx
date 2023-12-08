66412a8b8659434dab596e18a4a1b80bddbafee0
Johan Abde <abdejohan@gmail.com>
2023-12-08 18:11:19 +0100

    Combine home and scan page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -45,29 +45,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="scan"
-        options={{
-          title: 'Scan',
-          tabBarIcon: ({ color }) => (
-            <AntDesign name="scan1" size={24} color="black" />
-          ),
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
         name="two"
         options={{
           title: 'Settings',
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,157 @@
 import { StyleSheet, View } from 'react-native';
-import { Text } from 'react-native-paper';
-import { useContext } from 'react';
+import { Button, Text } from 'react-native-paper';
+import BottomSheet from '@gorhom/bottom-sheet';
+import {
+  useRef,
+  useMemo,
+  useCallback,
+  useState,
+  useEffect,
+  useContext,
+} from 'react';
+import { Ionicons } from '@expo/vector-icons';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 import StoreContext from '../../contexts/Store';
+import { checkForAllergens } from '../../utils';
+import Product from '../../components/Product';
 
-export default function TabOneScreen() {
-  const { searches } = useContext(StoreContext);
+export default function ScanScreen() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [matches, setMatches] = useState<string[]>([]);
+  const { fetchProduct, productData, user, setProductData, searches } =
+    useContext(StoreContext);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = async ({ data }: { data: string | null }) => {
+    setScanned(true);
+    bottomSheetRef.current?.close();
+    //  fetch product from the backend
+    data && fetchProduct(data);
+  };
+
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // variables
+  const snapPoints = useMemo(() => ['90%'], []);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
+  useEffect(() => {
+    if (user && productData) {
+      const allergenMatches = checkForAllergens(user?.allergens, productData);
+      setMatches(allergenMatches);
+    }
+  }, [user, productData]);
 
   return (
     <View style={styles.container}>
-      {searches?.map((product) => (
-        <Text key={product.gtin}>{product.name}</Text>
-      ))}
+      <View style={styles.productContainer}>
+        {!productData && (
+          <View style={{ alignItems: 'center', padding: 20 }}>
+            <Text variant="displaySmall">Ready to Scan</Text>
+            <Ionicons name="barcode-outline" size={100} color="black" />
+            <View style={styles.searches}>
+              <Text variant="labelLarge" style={{ marginBottom: 10 }}>
+                Recent searches
+              </Text>
+              {searches?.map((product) => (
+                <Text style={styles.searchItem} key={product.gtin}>
+                  {product.name}
+                </Text>
+              ))}
+            </View>
+          </View>
+        )}
+        {productData && <Product product={productData} matches={matches} />}
+      </View>
+      <Button
+        mode="contained"
+        style={styles.scanButton}
+        onPress={() => {
+          setScanned(false);
+          setProductData(null);
+          bottomSheetRef.current?.expand();
+        }}>
+        Scan
+      </Button>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        style={styles.bottomSheet}
+        onChange={handleSheetChanges}>
+        <View style={styles.contentContainer}>
+          <Text variant="headlineMedium">Aim camera at the barcode</Text>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={styles.barCodeScanner}
+          />
+          <Button
+            mode="contained"
+            onPress={() => bottomSheetRef.current?.close()}>
+            Return{' '}
+          </Button>
+        </View>
+      </BottomSheet>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 20,
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  searches: {
+    marginTop: 30,
+    width: '100%',
   },
+  searchItem: {
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+  },
+  bottomSheet: {
+    padding: 20,
+    paddingBottom: 200,
+    marginBottom: 200,
+  },
+  barCodeScanner: {
+    height: 200,
+    width: '100%',
+    marginVertical: 20,
+  },
+  productContainer: {
+    flex: 1,
+    width: '100%',
+    marginBottom: 20,
+  },
+
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  scanButton: {
+    marginBottom: 20,
   },
 });
