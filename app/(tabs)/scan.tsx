4df710dd613b951a690e2953c796c3d523429199
Johan Abde <abdejohan@gmail.com>
2023-12-08 10:14:44 +0100

    Integrate the scanner with bottom-sheet

--- app/(tabs)/scan.tsx
+++ app/(tabs)/scan.tsx
@@ -1,13 +1,57 @@
 import { StyleSheet } from 'react-native';
 import { Button } from 'react-native-paper';
-
+import BottomSheet from '@gorhom/bottom-sheet';
 import { Text, View } from '../../components/Themed';
+import { useRef, useMemo, useCallback, useState, useEffect } from 'react';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default function TabOneScreen() {
+export default function ScanScreen() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    bottomSheetRef.current?.close();
+  };
+
+  // ref
+  const bottomSheetRef = useRef<BottomSheet>(null);
+
+  // variables
+  const snapPoints = useMemo(() => ['90%'], []);
+
+  // callbacks
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log('handleSheetChanges', index);
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Scan Tab</Text>
-      <Button>Hej</Button>
+      <Button onPress={() => bottomSheetRef.current?.expand()}>Scan</Button>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        onChange={handleSheetChanges}>
+        <View style={styles.contentContainer}>
+          <Button onPress={() => bottomSheetRef.current?.close()}>Close</Button>
+          <Text>Awesome 🎉</Text>
+          <BarCodeScanner
+            onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+            style={StyleSheet.absoluteFillObject}
+          />
+        </View>
+      </BottomSheet>
     </View>
   );
 }
@@ -26,5 +70,9 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
   },
 });
